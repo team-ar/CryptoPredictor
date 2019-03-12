c25732a0718cba270c4a7e087488005ccba32c4e
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-03-12 11:55:55 +0100

    ayuda alberto apis

--- public/javascripts/apiCoinMarket.js
+++ public/javascripts/apiCoinMarket.js
@@ -2,21 +2,22 @@ const axios = require('axios')
 
 const apiCoinMarket = {
 
-  getAllCoins() {
-    return axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', { 'headers': { 'X-CMC_PRO_API_KEY': process.env.APIKEY } })
-      .then(response => response.data) // response.data.quotes.market_cap
-      .catch(error => console.log(error));
-     
-  },
+    url: "https://pro-api.coinmarketcap.com/v1/cryptocurrency",
 
-  getOneCoin(id) {
-    return axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${id}`, { 'headers': { 'X-CMC_PRO_API_KEY': process.env.APIKEY } })
-      .then(response => response.data) // response.data.quotes.market_cap
-      .catch(error => console.log(error));
+    getAllCoins() {
+        return axios.get(`${this.url}/listings/latest`, { 'headers': { 'X-CMC_PRO_API_KEY': process.env.APIKEY } })
+            .then(response => response.data) // response.data.quotes.market_cap
+            .catch(error => console.log(error));
 
-  }
+    },
+
+    getOneCoin(symbol) {
+        return axios.get(`https://min-api.cryptocompare.com/data/histoday?fsym=${symbol}&tsym=USD&limit=100`, { 'headers': { 'Apikey': process.env.APIKEYCRYPTOCOMPARE } })
+            .then(response => response.data) // response.data.quotes.market_cap
+            .catch(error => console.log(error));
+
+    }
 
 }
 
 module.exports = apiCoinMarket
-
--- routes/index.js
+++ routes/index.js
@@ -5,31 +5,31 @@ const apiCoinMarket = require("../public/javascripts/apiCoinMarket")
 /* GET home page */
 router.get('/', (req, res, next) => {
 
-  apiCoinMarket.getAllCoins()
+    apiCoinMarket.getAllCoins()
 
     .then(marketCoins => {
-      const marketCoins20 = marketCoins.data.slice(0, 20)
+        const marketCoins20 = marketCoins.data.slice(0, 20)
 
-      // console.log(marketCoins20)
-      res.render('index', { coins: marketCoins20 });
-      //
+        // console.log(marketCoins20)
+        res.render('index', { coins: marketCoins20 });
+        //
     })
 });
 
 router.get("/:id", (req, res) => {
 
-  // console.log(req.params.id)
+    // console.log(req.params.id)
 
-  const id = req.params.id
+    const id = req.params.id
 
-  apiCoinMarket.getOneCoin(id)
-  .then(marketCoin => {
-    res.json(marketCoin)
-  })
-  
+    apiCoinMarket.getOneCoin(id)
+        .then(marketCoin => {
+            console.log(marketCoin)
+        })
+
 })
 
 module.exports = router;
 
 
-https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC,ETH,XRP,BCH,EOS,LTC,XLM&convert=BTC,ETH,EUR
+// https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC,ETH,XRP,BCH,EOS,LTC,XLM&convert=BTC,ETH,EUR
