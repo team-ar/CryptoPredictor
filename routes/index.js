4d5e90259a8fa2270ce598f3405a3f7502015b8c
Raluca-Andreea <danaralucaandreea@gmail.com>
2019-03-13 17:19:52 +0100

    Final

--- routes/index.js
+++ routes/index.js
@@ -14,38 +14,20 @@ router.get('/', (req, res, next) => {
         const marketCoins20 = marketCoins.data.slice(0, 20)
 
         // console.log(marketCoins20)
-<<<<<<< HEAD
-        res.render('index', { coins: marketCoins20 });
-
-=======
         res.render('index', { coins: marketCoins20 });   
->>>>>>> 6eaf672e55eac543cb654df7dc0c3827ca228273
     })
 
-<<<<<<< HEAD
-/* GET Cryptocurrency page */
-=======
     .catch(err => {
         console.log('Error while finding the markerCoins', err)
         next()
     })
 });
 
->>>>>>> 6eaf672e55eac543cb654df7dc0c3827ca228273
 
 
 router.get("/coin/:id", (req, res, next) => {
 
     const id = req.params.id
-<<<<<<< HEAD
-
-    apiCoinMarket.getOneCoin(id)
-        .then(marketCoin => {
-            // res.json(marketCoin.data[id])
-            // res.json(marketCoin)
-            // res.render('cryptocurrency', { coins: JSON.stringify(marketCoin.Data), symbol: id });
-            res.render('cryptocurrency', { coins: JSON.stringify(marketCoin.Data), symbol: id, userId: req.session.currentUser !== undefined ? req.session.currentUser._id : "notLoggin" });
-=======
  
     apiCoinMarket.getOneCoin(id)
         .then(marketCoin => {
@@ -63,7 +45,6 @@ router.get("/coin/:id", (req, res, next) => {
                 next()
             })
 
->>>>>>> 6eaf672e55eac543cb654df7dc0c3827ca228273
         })
 
 })
