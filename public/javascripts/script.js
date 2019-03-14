e10f7f837f6574c933dce02baa74955c6d4b4b6c
Raluca-Andreea <danaralucaandreea@gmail.com>
2019-03-14 17:11:10 +0100

    Updated chert

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,31 +11,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginBox = document.getElementById("login-box")
     const signupBox = document.getElementById("signup-box")
 
-    // const cryptoCards = document.getElementsByClassName("crypto-cards")
-    // console.log(cryptoCards)
-    // cryptoCards.forEach(crypto => {
-    //     console.log("hola")
-    //     crypto.style.display = "inline-block"
-    // });
-
 
     if (login !== null) {
 
         login.onclick = e => {
-            if (loginBox.style.display === "block") loginBox.style.display = "none"
-            else loginBox.style.display = "block"
 
-            signupBox.style.display = "none"
+            //loginBox.style.transform = "translateX(-400px)"
+            
+            if (loginBox.style.transform === "translateX(-400px)") {
+                loginBox.style.transform = "translateX(400px)"
+            } else {
+                loginBox.style.transform = "translateX(-400px)"
+            }
+           
+            console.log(loginBox.style)
         }
     }
 
 
 
     const singupEvent = e => {
-        if (signupBox.style.display === "block") signupBox.style.display = "none"
-        else signupBox.style.display = "block"
 
-        loginBox.style.display = "none"
+        signupBox.style.transform = "translateX(-400px)"
+            
+            if (signupBox.style.right === "400px") {
+                signupBox.style.transform = "translateX(400px)"
+            }
+           
+            console.log(signupBox.style)
+        
+        
     }
 
     if (singup !== null) {
@@ -47,68 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     singup.onclick = singupEvent
-    singup2.onclick = singupEvent
+    // singup2.onclick = singupEvent
 
 
 
 }, false);
 
-
-//                        Original Variable
-
-// document.addEventListener('DOMContentLoaded', () => {
-
-//     const login = document.getElementById("login")
-//     const signup = document.getElementById("signup")
-
-//     const loginBox = document.getElementById("login-box")
-//     const signupBox = document.getElementById("signup-box")
-
-//     login.onclick = e => {
-//         if (loginBox.style.display === "block") loginBox.style.display = "none"
-//         else loginBox.style.display = "block"
-
-//         signupBox.style.display = "none"
-//     }
-
-//     signup.onclick = e => {
-//         if (signupBox.style.display === "block") signupBox.style.display = "none"
-//         else signupBox.style.display = "block"
-
-//         loginBox.style.display = "none"
-//     }
-
-// }, false);
-
-
-/////                           Log In Variable
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-
-//     const login = document.getElementById("login")
-//     const login2 = document.getElementById("login2")
-//     const signup = document.getElementById("signup")
-
-//     const loginBox = document.getElementById("login-box")
-//     const signupBox = document.getElementById("signup-box")
-
-//     const loginEvent = e => {
-//         if (loginBox.style.display === "block") loginBox.style.display = "none"
-//         else loginBox.style.display = "block"
-
-//         signupBox.style.display = "none"
-//     }
-
-//     login.onclick = loginEvent
-//     login2.onclick = loginEvent
-
-//     signup.onclick = e => {
-//         if (signupBox.style.display === "block") signupBox.style.display = "none"
-//         else signupBox.style.display = "block"
-
-//         loginBox.style.display = "none"
-//     }
-
-
-// }, false);
--- routes/index.js
+++ routes/index.js
@@ -13,34 +13,63 @@ router.get('/', (req, res, next) => {
     .then(marketCoins => {
         const marketCoins20 = marketCoins.data.slice(0, 20)
         const volumeBtc = parseInt(marketCoins20[0].quote.USD.market_cap)
-    
+      
         marketCoins20.forEach((coin, idx) => {
-            // console.log(idx)
+            
             if(coin.symbol === "BTC") {
-                if(coin.quote.USD.percent_change_24h >= 0 ) coin.color = "#FF9595"
-                else coin.color = "#439595"
+                if(coin.quote.USD.percent_change_24h >= 0 ) coin.color = "#058484"
+                else coin.color = "#F15659"
                 coin.dimension = 1
                 coin.width = 400
                 coin.height = 500
+                coin.fontSize = coin.dimension * 200
+                coin.fontSizePrice = 50
+                coin.fontSizeChange = 30
+                coin.gridColumn = `span 6`
+                coin.gridRow = `span 8`
             }
             else {
-                if(coin.quote.USD.percent_change_24h >= 0 ) coin.color = "#FF9595"
-                else coin.color = "#439595"
+                if(coin.quote.USD.percent_change_24h >= 0 ) coin.color = "#058484"
+                else coin.color = "#F15659"
                 const volumeCoin = parseInt(marketCoins20[idx].quote.USD.market_cap)
                 const dimension = volumeCoin / volumeBtc
                 coin.dimension = dimension * 8
                 coin.width = 400 * dimension 
                 coin.height = 500 * dimension
 
+                if(coin.dimension >= 0.04 && coin.dimension <= 0.15) {
+                    coin.gridColumn = `span 1`
+                    coin.gridRow = `span 2`
+                    coin.fontSize = 30
+
+                } else if (coin.dimension > 0.15 && coin.dimension <= 0.24) {
+                    coin.gridColumn = `span 2`
+                    coin.gridRow = `span 1`
+                    coin.fontSize = 35
+
+                } else if (coin.dimension > 0.24) {
+                    coin.gridColumn = `span 2`
+                    coin.gridRow = `span 2`
+                    coin.fontSize = 40
+
+                }
+
+
                 if (coin.width < 25) {
                     coin.width = 25
                     coin.height = 25
-                }
+                    }
             }
             
         });
 
-        // console.log(marketCoins20)
+        console.log(marketCoins20)
+
+        marketCoins20.map(coin => {
+            coin.quote.USD.price=parseFloat(coin.quote.USD.price).toFixed(2)
+            coin.quote.USD.percent_change_24h=parseFloat(coin.quote.USD.percent_change_24h).toFixed(2)
+        })
+
         res.render('index', { coins: marketCoins20 });   
     })
 
